e199ea4a9818cae2dcf669db71bb3bd45391fca2
Tim Muzzin <timmuzzin@Tims-iMac.local>
2017-09-26 15:34:04 -0600

    updating script name

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name='PythonSQLGenerator',    # This is the name of your PyPI-package.
     version='0.1',      # Update the version number for new releases
-    scripts=['python_sql_generator'] # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['python_sql_generator.py'] # The name of your scipt, and also the command you'll be using for calling it
 )
