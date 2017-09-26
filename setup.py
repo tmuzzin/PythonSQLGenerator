d6502aec111e3621f4726e19e4c3a3cd2dd22fa5
Tim Muzzin <timmuzzin@Tims-iMac.local>
2017-09-26 17:36:42 -0600

    cleaning up package for installation

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name='PythonSQLGenerator',    # This is the name of your PyPI-package.
     version='0.4',      # Update the version number for new releases
-    scripts=['PythonSQLGenerator'] # The name of your scipt, and also the command you'll be using for calling it
+    packages=['PythonSQLGenerator'] # The name of your scipt, and also the command you'll be using for calling it
 )
