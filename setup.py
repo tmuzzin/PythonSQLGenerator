f7b437937cebf8f76affb7dff2e6cc20341971db
Tim Muzzin <timmuzzin@Tims-iMac.local>
2017-09-26 17:02:25 -0600

    fixed package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(
     name='PythonSQLGenerator',    # This is the name of your PyPI-package.
-    version='0.2',      # Update the version number for new releases
-    scripts=['python_sql_generator.py'] # The name of your scipt, and also the command you'll be using for calling it
+    version='0.3',      # Update the version number for new releases
+    scripts=['PythonSQLGenerator'] # The name of your scipt, and also the command you'll be using for calling it
 )
